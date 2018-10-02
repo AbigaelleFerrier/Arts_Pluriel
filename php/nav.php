41007ba2221bb587b82460f8cb0414503fb02c19
Taira <cirill@asheart.fr>
2018-10-02 17:46:18 +0200

    design

---
+++ php/nav.php
@@ -0,0 +1,25 @@
+
+    <nav class="white nav-extended scrollspy" role="navigation" id="nav">
+
+        <div class="nav-wrapper container">
+            <a id="logo-container" href="#" class="brand-logo">Ferrier Cirill</a>
+            
+           <ul class="right hide-on-med-and-down">
+                <?php include 'sous-nav.php' ?>
+            </ul>
+
+            <ul id="nav-mobile" class="sidenav">
+                <?php include 'sous-nav.php' ?>
+            </ul>
+            <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a> 
+        </div>
+
+        <div class="nav-content container center">
+          <ul class="tabs tabs-transparent">
+            <li class="tab"> <a href="#PortFolio" class="js-scrollTo scrollspy">Projet</a></li>
+            <li class="tab"> <a href="#Stage"     class="js-scrollTo scrollspy">Stage</a></li>
+            <li class="tab"> <a href="">Test 3</a></li>
+            <li class="tab"> <a href="">Test 4</a></li>
+          </ul>
+        </div>
+    </nav>
