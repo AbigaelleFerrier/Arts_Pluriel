b5aace8853cbe2ab1ec22694977dcdd2b2d9c589
Taira <cirill@asheart.fr>
2018-10-15 16:57:59 +0200

    aa

--- creer_mon_compte.php
+++ creer_mon_compte.php
@@ -12,44 +12,60 @@
     	<div class="row">
     		<form class="col offset-s1 s10 offset-l7 l5 fondForm z-depth-4">
     			<h1 class="noMarge">S'inscrire</h1>
+    			<div class="row">
+			        <div class="input-field col s10">
+			          	<input id="pseudo" type="text" class="validate">
+			          	<label for="pseudo">Pseudo *</label>
+			        </div>
+			        <div id="verifPseudoAjax">
+			        	
+			        </div>
+			    </div>
 			    <div class="row">
 			        <div class="input-field col s6">
-			          	<input placeholder="Placeholder" id="first_name" type="text" class="validate">
-			          	<label for="first_name">First Name</label>
+			          	<input id="nom" type="text" class="validate">
+			          	<label for="nom">Nom *</label>
 			        </div>
 			        <div class="input-field col s6">
-			          	<input id="last_name" type="text" class="validate">
-			          	<label for="last_name">Last Name</label>
+			          	<input id="prenom" type="text" class="validate">
+			          	<label for="prenom">Prenom *</label>
 			        </div>
 			    </div>
+
 			    <div class="row">
-			        <div class="input-field col s12">
-			          	<input disabled value="I am not editable" id="disabled" type="text" class="validate">
-			          	<label for="disabled">Disabled</label>
-			        </div>
-			    </div>
-			    <div class="row">
-			        <div class="input-field col s12">
-			          	<input id="password" type="password" class="validate">
-			          	<label for="password">Password</label>
-			        </div>
-			    </div>
-			    <div class="row">
-			        <div class="input-field col s12">
+			    	<div class="input-field col s10">
 			          	<input id="email" type="email" class="validate">
-			          	<label for="email">Email</label>
+			          	<label for="email">Email *</label>
+			        </div>
+			        <div id="verifMailAjax">
+			        	
 			        </div>
 			    </div>
 			    <div class="row">
-			        <div class="col s12">
-			          	This is an inline input field:
-			          	<div class="input-field inline">
-			            	<input id="email_inline" type="email" class="validate">
-			            	<label for="email_inline">Email</label>
-			            	<span class="helper-text" data-error="wrong" data-success="right">Helper text</span>
-			          	</div>
+			        <div class="input-field col s6">
+			          	<input id="password" type="password" class="validate">
+			          	<label for="password">Mot de passe *</label>
+			        </div>
+			        <div class="input-field col s6">
+			          	<input id="passwordV" type="password" class="validate">
+			          	<label for="passwordV">Vérification Mot de passe *</label>
 			        </div>
 			    </div>
+			    <div class="row">
+			    	<div class="input-field col s6">
+			          	<input id="tel" type="text" class="validate">
+			          	<label for="tel">Téléphone (afficher publiquement)</label>
+			        </div>
+			        <div class="input-field col s6">
+			          	<input id="tel" type="text" class="validate">
+			          	<label for="tel">Ville (afficher publiquement)</label>
+			        </div>
+			        <div class="input-field col s12">
+			          	<input id="tel" type="text" class="validate">
+			          	<label for="tel">Distance de recherche (modifiable par la suite)</label>
+			        </div>
+			    </div>
+			    
 			    <button type="summit" class="btn right">Valider</button>
 			</form>
 			<a href="https://unsplash.com/" class="right">Image: unsplash.com</a>
