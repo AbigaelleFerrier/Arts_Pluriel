cc8b3f3233dd60451bbe13817740e1ad5f15416f
Taira <cirill@asheart.fr>
2018-10-17 11:01:17 +0200

    mycommit

--- classes/Utilisateur.class.php
+++ classes/Utilisateur.class.php
@@ -35,8 +35,8 @@
             $this->bioU				= $bioU;
             $this->LAT				= $LAT;
             $this->LONG				= $LONG;
-			$this->TabSousActivite	= $TabSousActivite;
-			$this->TabActivite		= $TabActivite;
+			//$this->TabSousActivite	= $TabSousActivite;
+			//$this->TabActivite		= $TabActivite;
 		}
 
 
@@ -96,11 +96,34 @@
 
 
 		public function getUtilisateursProches(){
-			$tabUsers = new array();
+			$tabUsers = array();
 
 			$manager = new UtilisateurManager(database::getDB());
 			$allUsers = $manager->getList("WHERE idU<>". $this->idU);
-			var_dump($allUsers);
+			
+			foreach ($allUsers as $key => $value) {
+				$meter = $this->distance($this->LAT, $this->LONG, $value->getLat(), $value->getLong());
+				
+				if ($this->distanceU >= $meter) {
+					$tabUsers[] = $value;
+				}
+			}
+			var_dump($tabUsers);
 		}
+
+		private function distance($lat1, $lng1, $lat2, $lng2) {
+	        $earth_radius = 6378137;   // Terre = sphère de 6378km de rayon
+	        $rlo1 = deg2rad($lng1);
+	        $rla1 = deg2rad($lat1);
+	        $rlo2 = deg2rad($lng2);
+	        $rla2 = deg2rad($lat2);
+	        $dlo = ($rlo2 - $rlo1) / 2;
+	        $dla = ($rla2 - $rla1) / 2;
+	        $a = (sin($dla) * sin($dla)) + cos($rla1) * cos($rla2) * (sin($dlo) * sin($dlo));
+	        $d = 2 * atan2(sqrt($a), sqrt(1 - $a));
+	        //
+	        $meter = ($earth_radius * $d);
+	        return $meter/1000;
+    	}
 
 	}
--- classes/index.php
+++ classes/index.php
@@ -1,16 +1,29 @@
 <?php
-    require ("classes/Activite.class.php");
-    require ("classes/Activite.Manager.php");
-    require ("classes/Admin.class.php");
-    require ("classes/Admin.Manager.php");
-    require ("classes/Lieux.class.php");
-    require ("classes/Lieux.Manager.php");
-    require ("classes/Message.class.php");
-    require ("classes/Message.Manager.php");
-    require ("classes/SousActivite.class.php");
-    require ("classes/SousActivite.Manager.php");
-    require ("classes/Utilisateur.class.php");
-    require ("classes/Utilisateur.Manager.php");
+    // require ("classes/Activite.class.php");
+    // require ("classes/Activite.Manager.php");
+    // require ("classes/Admin.class.php");
+    // require ("classes/Admin.Manager.php");
+    // require ("classes/Lieux.class.php");
+    // require ("classes/Lieux.Manager.php");
+    // require ("classes/Message.class.php");
+    // require ("classes/Message.Manager.php");
+    // require ("classes/SousActivite.class.php");
+    // require ("classes/SousActivite.Manager.php");
+    // require ("classes/Utilisateur.class.php");
+    // require ("classes/Utilisateur.Manager.php");
+    require ("Activite.class.php");
+    require ("Activite.Manager.php");
+    require ("Admin.class.php");
+    require ("Admin.Manager.php");
+    require ("Lieux.class.php");
+    require ("Lieux.Manager.php");
+    require ("Message.class.php");
+    require ("Message.Manager.php");
+    require ("SousActivite.class.php");
+    require ("SousActivite.Manager.php");
+    require ("Utilisateur.class.php");
+    require ("Utilisateur.Manager.php");
+
 ?>
 <!DOCTYPE html>
 <!--
@@ -25,33 +38,49 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+        
         // put your code here
-        echo "<br><br>TEST DE LA CLASSE UTILISATEUR <br>"; //IMPOSSIBLE A TESTER
-            echo "------------------------------------------------";
+        // echo "<br><br>TEST DE LA CLASSE UTILISATEUR <br>"; //IMPOSSIBLE A TESTER
+        //     echo "------------------------------------------------";
+            
+         echo '<pre>';
             $managerU = new UtilisateurManager(database::getDB());
-            echo"<pre>";
-                var_dump ($managerU->getList());
-            echo '</pre><pre>';
-                var_dump ($managerU->getList("WHERE idU<4"));
-            echo '</pre><pre>';
-                var_dump ($managerU->get(14));
-            echo '</pre>';
-            $UTest = new Utilisateur(45,"pseudo","nom", "prenom", "mail", "tel", "ville", "distance", "mdp","","","","");
+            // $allUsers = $managerU->getList("WHERE idU<>". 2);
+
+            // var_dump($allUsers);
+        echo "<pre>";
             
-            echo '<pre>';
-                var_dump ($managerU->save($UTest));
-            echo '</pre>';
+
+
+            // echo '</pre><pre>';
+                //     var_dump ($managerU->get(14));
+                // echo '</pre>';
+            //                      $id,$pseudoU, $nomU, $prenomU, $mailU, $telU, $villeU, $distanceU, $mdpU, $ddnU,$bioU,$LAT,$LONG
             
-            $UTest->setId(27);
-            $UTest->setPseudo("PseudoGenial");
-            echo '<pre>';
-                var_dump ($managerU->save($UTest));
-            echo '</pre>';
-            var_dump ($managerU->delete($UTest));
-            $UTest->setId(28);
-            echo '<pre>';
-                var_dump ($managerU->delete($UTest));
-            echo '</pre>';
+            $UTest1 = $managerU->get(2);
+            
+            $UTest1->getUtilisateursProches();
+
+
+            // $UTest2 = new Utilisateur(2,"pseudo2","nom", "prenom", "mail", "tel", "ville", 50        , "mdp", ""   ,""   , "" ,"");
+                    // $UTest3 = new Utilisateur(3,"pseudo3","nom", "prenom", "mail", "tel", "ville", 50        , "mdp", ""   ,""   , "" ,"");
+                    // $UTest4 = new Utilisateur(4,"pseudo4","nom", "prenom", "mail", "tel", "ville", 50        , "mdp", ""   ,""   , "" ,"");
+                    // $UTest5 = new Utilisateur(5,"pseudo5","nom", "prenom", "mail", "tel", "ville", 50        , "mdp", ""   ,""   , "" ,"");
+                    
+                    // echo '<pre>';
+                    //     var_dump ($managerU->save($UTest));
+                    // echo '</pre>';
+                    
+                    // $UTest->setId(27);
+                    // $UTest->setPseudo("PseudoGenial");
+                    // echo '<pre>';
+                    //     var_dump ($managerU->save($UTest));
+                    // echo '</pre>';
+                    // var_dump ($managerU->delete($UTest));
+                    // $UTest->setId(28);
+                    // echo '<pre>';
+                    //     var_dump ($managerU->delete($UTest));
+            // echo '</pre>';
             
         ?>
     </body>
