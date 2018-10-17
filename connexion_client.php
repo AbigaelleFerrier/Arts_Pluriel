5932a76e40c99c29553e40746fadbf4a549bf55c
Taira <cirill@asheart.fr>
2018-10-17 13:53:18 +0200

    aaa

--- connexion_client.php
+++ connexion_client.php
@@ -14,10 +14,9 @@ try {
     if ($ligne=$traitement->fetch()) {
         //alors il y a bien 1 résultat
             $_SESSION['user']       = "client";
-
         // on mes l'obj client de façon a pouvoir l'utiliser plus tard //
-            $user = $mana->get($ligne['idU']);
-            $user = serialize($user);
+            $userUS = $mana->get($ligne['idU']);
+            $user = serialize($userUS);
             $_SESSION['objUser']    = $user;
             $_SESSION['access']     = "limited";
         // ---------------------------------------------
@@ -29,6 +28,7 @@ try {
         $traitement2->bindparam(':m',$_POST['email']);
         $traitement2->bindparam(':p',$_POST['password']);
         $traitement2->execute();
+        
         if ($ligne=$traitement2->fetch()){
             $_SESSION['user']="admin";
             $_SESSION['access']="ok";
